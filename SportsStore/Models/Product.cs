eb3802a5c6efe471c300d3fc0286f3781db6d69a
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2023-09-30 16:15:04 +0200

    Admin's products table

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsStore.Models
@@ -5,10 +6,19 @@ namespace SportsStore.Models
     public class Product
     {
         public long? ProductId {get; set;}
+        [Required(ErrorMessage = "Please enter a product Name")]
         public string Name {get; set;} = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a decription")]
         public string Description {get; set;} = string.Empty;
+
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Please enter a positive price")]
         [Column(TypeName = "decimal(8,2)")]
         public decimal Price {get; set;}
+
+        [Required(ErrorMessage = "PLease specify a category")]
         public string Category {get; set;} = string.Empty;
     }
 }
